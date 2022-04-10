4454920eb1e045579ea8a12f66914902bcc89a10
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-04-10 22:22:47 +0530

    script in smart_contract

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -20,13 +20,16 @@ contract Transactions{
 
     function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public{
         transactionCount += 1;
+        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword)); //pushing a specific
+        //transaction into the transactions array
+        emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword); //to transfer
     } 
 
     function getAllTransactions() public view returns (TransferStruct[] memory){ //return transactions
-
+        return transactions;
     }
 
     function getTransactionCount() public view returns (uint256){ //return transaction count 
-
+        return transactionCount;
     }
 }
